6c988b87a5a49c46eb4e502cc79cb83a7b812f48
waamber <amberastuart@aol.com>
2018-06-20 21:07:14 -0500

    final

--- MyDailyQuote/Services/RandomQuote.cs
+++ MyDailyQuote/Services/RandomQuote.cs
@@ -17,7 +17,7 @@ namespace MyDailyQuote.Services
 		{
 			
 			var accountSid = "ACb683348bef9a93aa101862bcccbd3a68";
-			var authToken = "a3df6a9a497fce3c109be06d6d8e9e54";
+			var authToken = "";
 			TwilioClient.Init(accountSid, authToken);
 
 			var repo = new QuoteRepo();
